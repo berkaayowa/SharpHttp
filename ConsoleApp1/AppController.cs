f0e207c7930965a2b92fce7b3799a2e814291607
berka <ayowaberka@gmail.com>
2022-10-25 00:28:57 +0200

    Add project files.

---
+++ ConsoleApp1/AppController.cs
@@ -0,0 +1,27 @@
+﻿using SharpHttpServer.Attribute;
+using SharpHttpServer.Controller;
+using SharpHttpServer.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AppController : SharpHttpServerController 
+    {
+        public SharpHttpServerResult Help()
+        {
+            return Text("In app->help function...");
+        }
+
+        //Post Only fuction
+        //this function will only be availabel on post request
+        [RouteSharpHttp(SharpHttpMethod.POST)]
+        public SharpHttpServerResult Command()
+        {
+            return Text("In app->Command function...");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using SharpHttpServer.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("########################################");
+            Console.WriteLine("##### Welocme to this console app ######");
+            Console.WriteLine("########################################");
+            Console.WriteLine("# This console app has an Http Server  #");
+            Console.WriteLine("# Embed so that we can expose certain  #");
+            Console.WriteLine("# frunctionality to third party apps   #");
+            Console.WriteLine("########################################");
+
+
+            //Set up Log envent
+            SharpHttp.Server.OnEvent += Log;
+
+            //Setup & run the Server 
+            SharpHttp.Server.Run("http://localhost:8091/", typeof(Program)).AlwaysOn();
+        }
+
+        //This sis basic log function 
+        //We are just writing logs on console
+        static void Log(SharpHttpLogSource source, SharpHttpLogType logType, object obj)
+        {
+            //Console.Clear();
+            Console.WriteLine(DateTime.Now.ToString() + "-" + obj.ToString());
+            
+        }
+    }
+}
